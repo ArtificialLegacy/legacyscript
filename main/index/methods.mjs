327dac6a3cbd34615d503baaf3e635c68acc7c53
ArtificialLegacy <32146549+ArtificialLegacy@users.noreply.github.com>
2018-10-19 21:06:03 -0400

    Added missing methods

--- main/index/methods.mjs
+++ main/index/methods.mjs
@@ -6,11 +6,15 @@ const syntax = {
   "print.stime": true,
   "print.etime": true,
   "print.clear": true,
+  "print.warn": true,
+  "print.info": true,
+  "print.error": true,
   "edit.round": true,
   "edit.floor": true,
   "edit.ceil": true,
   "edit.flip": true,
   "edit.abs": true,
+  "edit.del": true,
   "construct.list": true,
   "construct.map": true,
   "index.list": true,
@@ -18,6 +22,7 @@ const syntax = {
   "set.random": true,
   "set.min": true,
   "set.max": true,
+  "set.sqr": true,
   "if": true,
 };
 
